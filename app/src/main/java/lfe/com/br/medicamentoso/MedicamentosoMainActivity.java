ea6852e0da08ef4c746ae522fccae40cfc455259
Leandro Fé <alundra_3@hotmail.com>
2017-05-13 15:59:07 -0300

    Initial commit

---
+++ app/src/main/java/lfe/com/br/medicamentoso/MedicamentosoMainActivity.java
@@ -0,0 +1,13 @@
+package lfe.com.br.medicamentoso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MedicamentosoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicamentoso_main);
+    }
+}
